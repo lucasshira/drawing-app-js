3aca2a4eacdf6dd56c3932a5df6281923da65168
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2023-12-01 16:39:08 -0300

    finishing the project

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,58 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
+const colorEl = document.getElementById('color');
+const sizeEl = document.getElementById('size');
+const clearEl = document.getElementById('clear');
 
 let size = 20;
 let isPressed = false
+let color = 'black';
+let x = undefined;
+let y = undefined;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
     isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+
 });
 
-canvas.addEventListener('mouseup', () => {
+canvas.addEventListener('mouseup', (e) => {
     isPressed = false;
+
+    x = undefined;
+    y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
     if (isPressed) {
-        const x = e.offsetX;
-        const y = e.offsetY;
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x, y);
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     }
 });
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
 };
 
-drawCircle(50, 50);
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+}
 
 increaseBtn.addEventListener('click', () => {
     size += 5;
@@ -37,6 +61,8 @@ increaseBtn.addEventListener('click', () => {
     if (size > 50) {
         size = 50;
     }
+
+    upateSizeOnScreen();
 });
 
 decreaseBtn.addEventListener('click', () => {
@@ -45,14 +71,18 @@ decreaseBtn.addEventListener('click', () => {
     if (size < 5) {
         size = 5;
     }
+
+    upateSizeOnScreen();
 });
 
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value;
+});
 
-//     drawCircle(x, y);
+clearEl.addEventListener('click', (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
-//     requestAnimationFrame(draw);
-// }
-
-// draw();
+function upateSizeOnScreen() {
+    sizeEl.innerText = size;
+};
